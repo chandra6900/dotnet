bf540ce912ed0ca12cd762d213af36f736546dc4
chandra6900 <chandra6900@gmail.com>
2018-01-28 12:13:27 +0530

    webreqestor prj cmt
    commiting first time

---
+++ console/WebRequest/WebRequest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace WebRequestor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest req = WebRequest.Create("http://microsoft.com");
+            WebResponse resp = req.GetResponse();
+            StreamReader reader = new StreamReader(resp.GetResponseStream(), Encoding.ASCII);
+            Console.WriteLine(reader.ReadToEnd());
+            Console.ReadKey();
+        }
+    }
+}
